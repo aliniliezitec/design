8ccfb6126ca3eb4c3f1ce6db1732be864276407b
Florin Galan <florin@naologic.com>
2019-12-19 16:02:58 +0200

    Added sanitizer pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     AppRoutingModule,
     LayoutModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,31 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbDateAdapter, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedService } from './shared.service';
+import { PrismHtml } from './pipes/prism-html.pipe';
 
 @NgModule({
   providers: [
     SharedService,
   ],
   declarations: [
+    PrismHtml
   ],
   imports: [
     CommonModule,
     NgbModule,
     FormsModule,
     ReactiveFormsModule,
-
-
   ],
   exports: [
     CommonModule,
     NgbModule,
     FormsModule,
     ReactiveFormsModule,
-
-
-
+    PrismHtml
   ],
   entryComponents: [
   ]
